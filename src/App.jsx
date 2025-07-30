6434f02ac8babc80fe9dd0a7f3fd2fd256a0e0a6
simone-poli <polismn97@gmail.com>
2025-07-30 15:57:27 +0200

    fix bugs and add logo title

--- src/App.jsx
+++ src/App.jsx
@@ -40,60 +40,66 @@ function App() {
 
   return (
     <>
-      <header>
-        <div className="container text-center">
-          <h1>Boolflix</h1>
+      <header className="bg-dark">
+        <div className="container d-flex justify-content-between align-items-center">
+          <img src="https://fontmeme.com/permalink/250730/3c20dfd41f94290e511eca21370fea4c.png" alt="" />
+          <form onSubmit={handleSubmit}>
+            <div className="container d-flex justify-content-center align-items-center">
+              <input
+                className="p-2"
+                type="text"
+                placeholder='Search title'
+                value={movieTitle}
+                onChange={(e) => setMovieTitle(e.target.value)} />
+              <button className="btn btn-outline-light btn-sm mx-3">Search</button>
+            </div>
+          </form>
         </div>
       </header>
 
       <main>
-        <form onSubmit={handleSubmit}>
-          <div className="container mt-4 d-flex">
-            <input
-              className="form-control"
-              type="text"
-              placeholder='Search title'
-              value={movieTitle}
-              onChange={(e) => setMovieTitle(e.target.value)} />
-            <button className="btn btn-primary mx-3">Search</button>
-          </div>
-        </form>
+        <div className="container-fluid my-3">
+          <div className="card border-0 p-0">
+            <div className="row row-cols-1 row-cols-md-4">
+              {movies.map((movie) => {
+                const countryCode = languageFlags(movie.original_language);
+                const vote = Math.ceil(movie.vote_average / 2);
 
-        <div className="container my-3">
-          <ul>
-            {movies.map((movie) => {
-              const countryCode = languageFlags(movie.original_language);
-              return (
-                <li key={movie.id} className="mb-3">
-                  Title: {movie.title || movie.name} <br />
-                  Original Title: {movie.original_title || movie.original_name} <br />
-                  Language:{' '}
-                  {countryCode && (
-                    <CountryFlag
-                      countryCode={countryCode}
-                      svg
-                      style={{ width: '1.5em', height: '1.5em', marginRight: '0.3em' }}
-                    />
-                  )}
-                  ({movie.original_language}) <br />
-                  Vote:
-                  {(() => {
-                    const vote = Math.ceil(movie.vote_average / 2);
-                    return (
-                      <>
-                        <i className={`fas fa-star ${vote >= 1 ? 'text-warning' : 'text-muted'}`}></i>
-                        <i className={`fas fa-star ${vote >= 2 ? 'text-warning' : 'text-muted'}`}></i>
-                        <i className={`fas fa-star ${vote >= 3 ? 'text-warning' : 'text-muted'}`}></i>
-                        <i className={`fas fa-star ${vote >= 4 ? 'text-warning' : 'text-muted'}`}></i>
-                        <i className={`fas fa-star ${vote >= 5 ? 'text-warning' : 'text-muted'}`}></i>
-                      </>
-                    );
-                  })()} <br/>
-                  Image: <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
-                </li>
-              )
-            })}
-          </ul>
+                return (
+                  <div className="col" key={movie.id}>
+                    <div className="card border-0">
+                      <div>
+                        <img className=" position-relative object-fit-cover z-1 pt-3" src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
+                      </div>
+                      <div className="position-absolute top-0 left-50 pt-3">
+                        <b>Title</b>: {movie.title || movie.name} <br />
+                        <b>Original Title</b>: {movie.original_title || movie.original_name} <br />
+                        <b>Language</b>:{' '}
+                        {countryCode && (
+                          <CountryFlag
+                            countryCode={countryCode}
+                            svg
+                            style={{ width: '1.5em', height: '1.5em', marginRight: '0.3em' }}
+                          />
+                        )}
+                        ({movie.original_language}) <br />
+                        <b>Vote</b>:
+                        <>
+                          <i className={`fas fa-star ${vote >= 1 ? 'text-warning' : 'text-muted'}`}></i>
+                          <i className={`fas fa-star ${vote >= 2 ? 'text-warning' : 'text-muted'}`}></i>
+                          <i className={`fas fa-star ${vote >= 3 ? 'text-warning' : 'text-muted'}`}></i>
+                          <i className={`fas fa-star ${vote >= 4 ? 'text-warning' : 'text-muted'}`}></i>
+                          <i className={`fas fa-star ${vote >= 5 ? 'text-warning' : 'text-muted'}`}></i>
+                        </>
+                        <br />
+                        <p className='text-break'> <b>Overview</b>: {movie.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </main>
     </>
